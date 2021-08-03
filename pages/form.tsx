1c0cbbb57681adbba750c0c5c5a0d21fbb84b0b3
rainkinz <brendan.grainger@gmail.com>
2021-08-03 14:46:27 +1200

    building out form example

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,7 +1,11 @@
+import { EmailIcon, InfoIcon, LockIcon } from "@chakra-ui/icons"
 import {
   Button,
+  Divider,
   FormControl,
-  Icon,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
   Input,
   InputGroup,
   InputLeftElement,
@@ -12,12 +16,36 @@ export default function FormExample() {
   return (
     <form action="submit">
       <Stack space={3} >
-        <InputGroup>
-          <InputLeftElement children={<Icon name="info" />} />
-          <Input type="name" placeholder="First Name" aria-label="First name" />
-        </InputGroup>
-        <Input />
-        <Input />
+        <FormControl isRequired>
+          <FormLabel>First name</FormLabel>
+          <InputGroup>
+            <InputLeftElement children={<InfoIcon />} />
+            <Input type="name" placeholder="First Name" aria-label="First name" />
+          </InputGroup>
+        </FormControl>
+        <FormControl isRequired>
+          <InputGroup>
+            <InputLeftElement children={<InfoIcon />} />
+            <Input type="name" placeholder="Last Name" aria-label="Last name" />
+          </InputGroup>
+        </FormControl>
+        <Divider />
+        <FormControl isRequired>
+          <InputGroup>
+            <InputLeftElement children={<EmailIcon />} />
+            <Input type="email" placeholder="email@email.com" aria-label="Email" />
+          </InputGroup>
+          <FormHelperText>
+            We never share your email
+          </FormHelperText>
+        </FormControl>
+        <FormControl isRequired isInvalid={true}>
+          <InputGroup>
+            <InputLeftElement children={<LockIcon />} />
+            <Input type="password" placeholder="Password" aria-label="Password" />
+          </InputGroup>
+          <FormErrorMessage>Password is required</FormErrorMessage>
+        </FormControl>
         <Button
           type="submit"
           variant="solid"
@@ -25,6 +53,9 @@ export default function FormExample() {
         >
           Sign Up
         </Button>
+        <FormControl>
+
+        </FormControl>
       </Stack>
     </form>
   )
